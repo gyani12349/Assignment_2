5f24f290fe7f7ba77ec8b245b68af49e3803b87f
Abhigyan Bhadra <bhadraabhigyan@gmail.com>
2019-09-02 21:59:01 +0530

    Add files via upload

--- Euler_4.java
+++ Euler_4.java
@@ -2,15 +2,27 @@ class Euler_4
  {
  	public static void main(String args[])
  	{ 
- 		double prod=0,i,j;
+ 		int tprod=0,prod=0,i,j,r,rev=0,palin=0;
  		for(i=100;i<1000;i++)
  		{
  		 for(j=100;j<1000;j++)
- 		 {
-  			if(i*j>prod)
-  			 prod=i*j;
+ 	  {
+ 	  	prod=i*j;
+ 	  	tprod=i*j;
+  		while(tprod>0)
+  		{
+  			r=tprod%10;
+  			rev=rev*10+r;
+  			tprod/=10;
+  		}
+  		if(rev==prod)
+  		{
+  			if(prod>palin)
+  			 palin=prod;	
+  		}
+  		rev=0;
   		}
  		}
- 		System.out.println("Greatest no:"+prod);
+ 		System.out.println("Greatest no:"+palin);
  	}
  }
